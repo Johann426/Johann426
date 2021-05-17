a6939e8fb8e553359244dda933cfcb7db86286e1
Johann <johannd0426@gmail.com>
2021-05-17 14:47:32 +0900

    Update react_test.js

--- react_test.js
+++ react_test.js
@@ -1,9 +1,32 @@
 class Car extends React.Component {
-	constructor() {
-		super();
-		this.state = {color: "red"};
-	}
-	render() {
-		return <h2> I am a {this.state.color} Car! </h2>;
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      brand: "Ford",
+      model: "Mustang",
+      color: "red",
+      year: 1964
+    };
+  }
+  changeColor = () => {
+    this.setState({color: "blue"});
+  }
+  render() {
+    return (
+      <div>
+        <h1>My {this.state.brand}</h1>
+        <p>
+          It is a {this.state.color}
+          {this.state.model}
+          from {this.state.year}.
+        </p>
+        <button
+          type="button"
+          onClick={this.changeColor}
+        >Change color</button>
+      </div>
+    );
+  }
 }
+
+ReactDOM.render(<Car/>, document.getElementById('root'));
