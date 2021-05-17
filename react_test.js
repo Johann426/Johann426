0cd38abd0bd64cb67cf942fad348445574b832f8
Johann <johannd0426@gmail.com>
2021-05-17 14:22:56 +0900

    Update react_test.js

--- react_test.js
+++ react_test.js
@@ -1,7 +1,9 @@
 class Car extends React.Component {
-  render() {
-    return <h2>Hi, I am a Car!</h2>;
-  }
+	constructor() {
+		super();
+		this.state = {color: "red"};
+	}
+	render() {
+		return <h2> I am a {this.state.color} Car! </h2>;
+	}
 }
-
-ReactDOM.render(<Car />, document.getElementById('root'));
