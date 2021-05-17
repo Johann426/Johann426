219421d5e93f5f68db0a901a34bc9269902c707e
Johann <johannd0426@gmail.com>
2021-05-17 16:44:18 +0900

    Update react_test.js

--- react_test.js
+++ react_test.js
@@ -8,23 +8,19 @@ class Car extends React.Component {
       year: 1964
     };
   }
-  changeColor = () => {
-    this.setState({color: "blue"});
+  changeHandler = (event) => {
+    this.setState({username: event.target.value});
   }
   render() {
     return (
-      <div>
-        <h1>My {this.state.brand}</h1>
-        <p>
-          It is a {this.state.color}
-          {this.state.model}
-          from {this.state.year}.
-        </p>
-        <button
-          type="button"
-          onClick={this.changeColor}
-        >Change color</button>
-      </div>
+      <form>
+      <h1>Hello {this.state.brand} {this.state.model} {this.state.color} </h1>
+      <p>Enter your brand:</p>
+      <input
+        type='text'
+        onChange={this.changeHandler}
+      />
+      </form>
     );
   }
 }
