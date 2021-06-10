f2012c53cfd495deaf5d57506ac46a973bf73f63
Johann <johannd0426@gmail.com>
2021-06-10 15:32:52 +0900

    Update tfjs.js

--- tfjs.js
+++ tfjs.js
@@ -6,8 +6,8 @@ async function getData() {
   const carsDataResponse = await fetch('https://storage.googleapis.com/tfjs-tutorials/carsData.json');
   const carsData = await carsDataResponse.json();
   const cleaned = carsData.map(car => ({
-    mpg: car.Miles_per_Gallon,
-    horsepower: car.Horsepower,
+	mpg: car.Miles_per_Gallon,
+	horsepower: car.Horsepower,
   }))
   .filter(car => (car.mpg != null && car.horsepower != null));
 
@@ -18,18 +18,18 @@ async function run() {
   // Load and plot the original input data that we are going to train on.
   const data = await getData();
   const values = data.map(d => ({
-    x: d.horsepower,
-    y: d.mpg,
+	x: d.horsepower,
+	y: d.mpg,
   }));
 
   tfvis.render.scatterplot(
-    {name: 'Horsepower v MPG'},
-    {values},
-    {
-      xLabel: 'Horsepower',
-      yLabel: 'MPG',
-      height: 300
-    }
+	{name: 'Horsepower v MPG'},
+	{values},
+	{
+	  xLabel: 'Horsepower',
+	  yLabel: 'MPG',
+	  height: 300
+	}
   );
 
   // Convert the data to a form we can use for training.
@@ -39,11 +39,11 @@ async function run() {
   // Train the model
   await trainModel(model, inputs, labels);
   console.log('Done Training');
-  
+
   // Make some predictions using the model and compare them to the
   // original data
   testModel(model, data, tensorData);
-  
+
 }
 
 document.addEventListener('DOMContentLoaded', run);
@@ -55,9 +55,12 @@ function createModel() {
   // Add a single input layer
   model.add(tf.layers.dense({inputShape: [1], units: 1, useBias: true}));
 
+  //hidden layer
+  model.add(tf.layers.dense({units: 50, activation: 'sigmoid'}));
+
   // Add an output layer
   model.add(tf.layers.dense({units: 1, useBias: true}));
-  
+
   return model;
 }
 
@@ -76,57 +79,57 @@ function convertToTensor(data) {
   // intermediate tensors.
 
   return tf.tidy(() => {
-    // Step 1. Shuffle the data
-    tf.util.shuffle(data);
+	// Step 1. Shuffle the data
+	tf.util.shuffle(data);
 
-    // Step 2. Convert data to Tensor
-    const inputs = data.map(d => d.horsepower)
-    const labels = data.map(d => d.mpg);
+	// Step 2. Convert data to Tensor
+	const inputs = data.map(d => d.horsepower)
+	const labels = data.map(d => d.mpg);
 
-    const inputTensor = tf.tensor2d(inputs, [inputs.length, 1]);
-    const labelTensor = tf.tensor2d(labels, [labels.length, 1]);
+	const inputTensor = tf.tensor2d(inputs, [inputs.length, 1]);
+	const labelTensor = tf.tensor2d(labels, [labels.length, 1]);
 
-    //Step 3. Normalize the data to the range 0 - 1 using min-max scaling
-    const inputMax = inputTensor.max();
-    const inputMin = inputTensor.min();
-    const labelMax = labelTensor.max();
-    const labelMin = labelTensor.min();
+	//Step 3. Normalize the data to the range 0 - 1 using min-max scaling
+	const inputMax = inputTensor.max();
+	const inputMin = inputTensor.min();
+	const labelMax = labelTensor.max();
+	const labelMin = labelTensor.min();
 
-    const normalizedInputs = inputTensor.sub(inputMin).div(inputMax.sub(inputMin));
-    const normalizedLabels = labelTensor.sub(labelMin).div(labelMax.sub(labelMin));
+	const normalizedInputs = inputTensor.sub(inputMin).div(inputMax.sub(inputMin));
+	const normalizedLabels = labelTensor.sub(labelMin).div(labelMax.sub(labelMin));
 
-    return {
-      inputs: normalizedInputs,
-      labels: normalizedLabels,
-      // Return the min/max bounds so we can use them later.
-      inputMax,
-      inputMin,
-      labelMax,
-      labelMin,
-    }
+	return {
+	  inputs: normalizedInputs,
+	  labels: normalizedLabels,
+	  // Return the min/max bounds so we can use them later.
+	  inputMax,
+	  inputMin,
+	  labelMax,
+	  labelMin,
+	}
   });
 }
 
 async function trainModel(model, inputs, labels) {
   // Prepare the model for training.
   model.compile({
-    optimizer: tf.train.adam(),
-    loss: tf.losses.meanSquaredError,
-    metrics: ['mse'],
+	optimizer: tf.train.adam(),
+	loss: tf.losses.meanSquaredError,
+	metrics: ['mse'],
   });
 
   const batchSize = 32;
   const epochs = 50;
 
   return await model.fit(inputs, labels, {
-    batchSize,
-    epochs,
-    shuffle: true,
-    callbacks: tfvis.show.fitCallbacks(
-      { name: 'Training Performance' },
-      ['loss', 'mse'],
-      { height: 200, callbacks: ['onEpochEnd'] }
-    )
+	batchSize,
+	epochs,
+	shuffle: true,
+	callbacks: tfvis.show.fitCallbacks(
+	  { name: 'Training Performance' },
+	  ['loss', 'mse'],
+	  { height: 200, callbacks: ['onEpochEnd'] }
+	)
   });
 }
 
@@ -138,38 +141,38 @@ function testModel(model, inputData, normalizationData) {
   // that we did earlier.
   const [xs, preds] = tf.tidy(() => {
 
-    const xs = tf.linspace(0, 1, 100);
-    const preds = model.predict(xs.reshape([100, 1]));
+	const xs = tf.linspace(0, 1, 100);
+	const preds = model.predict(xs.reshape([100, 1]));
 
-    const unNormXs = xs
-      .mul(inputMax.sub(inputMin))
-      .add(inputMin);
+	const unNormXs = xs
+	  .mul(inputMax.sub(inputMin))
+	  .add(inputMin);
 
-    const unNormPreds = preds
-      .mul(labelMax.sub(labelMin))
-      .add(labelMin);
+	const unNormPreds = preds
+	  .mul(labelMax.sub(labelMin))
+	  .add(labelMin);
 
-    // Un-normalize the data
-    return [unNormXs.dataSync(), unNormPreds.dataSync()];
+	// Un-normalize the data
+	return [unNormXs.dataSync(), unNormPreds.dataSync()];
   });
 
 
   const predictedPoints = Array.from(xs).map((val, i) => {
-    return {x: val, y: preds[i]}
+	return {x: val, y: preds[i]}
   });
 
   const originalPoints = inputData.map(d => ({
-    x: d.horsepower, y: d.mpg,
+	x: d.horsepower, y: d.mpg,
   }));
 
 
   tfvis.render.scatterplot(
-    {name: 'Model Predictions vs Original Data'},
-    {values: [originalPoints, predictedPoints], series: ['original', 'predicted']},
-    {
-      xLabel: 'Horsepower',
-      yLabel: 'MPG',
-      height: 300
-    }
+	{name: 'Model Predictions vs Original Data'},
+	{values: [originalPoints, predictedPoints], series: ['original', 'predicted']},
+	{
+	  xLabel: 'Horsepower',
+	  yLabel: 'MPG',
+	  height: 300
+	}
   );
 }
