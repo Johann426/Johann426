3f7229b3868356bb36478e4a75ce40373ddc0d3c
Johann <johannd0426@gmail.com>
2021-09-09 09:33:01 +0900

    Update cad_curves.js

--- cad_curves.js
+++ cad_curves.js
@@ -374,7 +374,7 @@ function init() {
 	loader.load( './3dm/Rhino_Logo.3dm', function ( object ) {
 
 		scene.add( object );
-		initGUI( object.userData.layers );
+		initGUI( object.userData.layers, scene );
 
 		// hide spinner
 		document.getElementById( 'loader' ).style.display = 'none';
@@ -599,5 +599,46 @@ function updateSelectedPoint( point, v ) {
 	arr[ 0 ] = v.x;
 	arr[ 1 ] = v.y;
 	arr[ 2 ] = v.z;
+
+}
+
+function initGUI( layers, scene ) {
+
+	const gui = new GUI( { width: 300 } );
+
+	const layersControl = gui.addFolder( 'layers' );
+	layersControl.open();
+
+	for ( let i = 0; i < layers.length; i ++ ) {
+
+		const layer = layers[ i ];
+		layersControl.add( layer, 'visible' ).name( layer.name ).onChange( function ( val ) {
+
+			const name = this.object.name;
+
+			scene.traverse( function ( child ) {
+
+				if ( child.userData.hasOwnProperty( 'attributes' ) ) {
+
+					if ( 'layerIndex' in child.userData.attributes ) {
+
+						const layerName = layers[ child.userData.attributes.layerIndex ].name;
+
+						if ( layerName === name ) {
+
+							child.visible = val;
+							layer.visible = val;
+
+						}
+
+					}
+
+				}
+
+			} );
+
+		} );
+
+	}
 
 }
