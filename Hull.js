e129183a84b7fffee54a31bc593c5d629a8277e8
Johann <johannd0426@gmail.com>
2021-12-31 11:20:19 +0900

    Create Hull.js

---
+++ Hull.js
@@ -0,0 +1,22 @@
+class Hull {
+
+	constructor() {
+	}
+
+	readTxt( txt ) {
+
+		const arr = txt.split( '\r\n' );
+		this.dia = Number( arr[ 4 ].split( /\s+/ )[ 1 ] );
+		this.NoBlade = Number( arr[ 4 ].split( /\s+/ )[ 2 ] );
+		this.HubD_face = Number( arr[ 5 ].split( /\s+/ )[ 1 ] );
+		this.rbyR = arr[ 6 ].split( /\s+/ ).map( e => Number( e ) );
+		this.rbyR.splice( 0, 1 );
+
+	}
+	
+	getCurves() {
+	}
+
+}
+
+export { Hull };
