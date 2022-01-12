ed6d085b289eef0376c41ec616e3744a55bf69cd
Johann <johannd0426@gmail.com>
2022-01-12 19:47:52 +0900

    clean up

--- Modeling/BezierCurve.js
+++ Modeling/BezierCurve.js
@@ -6,51 +6,51 @@ class BezierCurve extends Parametric {
 	constructor() {
 
 		super();
-		
+
 		this.ctrlp = [];
 
 	}
 
 	get ctrlPoints() {
-	
+
 		return this.ctrlp;
 
 	}
-	
+
 	get designPoints() {
-		
+
 		return this.ctrlp;
 
 	}
-	
+
 	add( v ) {
-		
+
 		this.ctrlp.push( v );
 
 	}
 
 	mod( i, v ) {
 
-		this.ctrlp[ i ] =v;
+		this.ctrlp[ i ] = v;
 
 	}
-	
+
 	getPoints( n ) {
 
 		const p = [];
-		
-		for ( let i =0; i < n; i ++ ) {
+
+		for ( let i = 0; i < n; i ++ ) {
 
 			const t = i / ( n - 1 );
 			p[ i ] = this.getPointAt( t );
 
 		}
-		
+
 		return p;
 
 	}
 
-	getPointAt ( t ) {
+	getPointAt( t ) {
 
 		return pointOnBezierCurve( this.ctrlp, t );
 
