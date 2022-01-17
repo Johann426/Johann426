ecfa0daf6f69b1030ccf1fd9c3d7a054bd34ae2f
Johann <johannd0426@gmail.com>
2022-01-17 10:12:59 +0900

    Update AddTangentCommand.js

--- commands/AddTangentCommand.js
+++ commands/AddTangentCommand.js
@@ -2,16 +2,19 @@ import { updateBuffer, updateLines } from '../Editor.js';
 
 class AddTangentCommand {
 
-	constructor( buffer ) {
+	constructor( buffer, index, tangent ) {
 
 		this.buffer = buffer;
-
+		this.index = index;
+		this.tangent = tangent;
 
 	}
 
 	execute() {
 
-		
+		const buffer = this.buffer;
+		const curve = buffer.pickable.selected.curve;
+		curve.addTangent( this.index, this.tangent );
 
 		updateBuffer( curve, buffer );
 		updateLines( curve, buffer.pickable.selected );
@@ -20,7 +23,9 @@ class AddTangentCommand {
 
 	undo() {
 
-		
+		const buffer = this.buffer;
+		const curve = buffer.pickable.selected.curve;
+		curve.removeTangent( this.index );
 
 		updateBuffer( curve, buffer );
 		updateLines( curve, buffer.pickable.selected );
